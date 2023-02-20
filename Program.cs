f5f629bf26cc1a1259a04feeca616f0d1701d498
Ece Unsal Karakus <eceunsal05@gmail.com>
2023-02-20 16:03:22 +0300

    islemli atama

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Atama ve İşlemli Atama
+        int x = 3;
+        int y = 3;
+        y = y + 2;
+        Console.WriteLine(y);
+        y += 2; //y eşittir y artı 2 olarak yazmak yerine böyle ifade edebiliriz.
+        Console.WriteLine(y);
+        y /=1;
+        Console.WriteLine(y);
+        x *= 2;
+        Console.WriteLine(x);
+    }
+}
